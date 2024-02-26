a3116ba18e6e4c27d784632eecba100c505fe477
황금 바바나 <63435073+woo677@users.noreply.github.com>
2024-02-26 23:54:33 +0900

    Java
    Java기본

---
+++ Define_자바정의.java
@@ -0,0 +1,34 @@
+package ex01;
+
+public class Define_자바정의 {
+
+	public static void main(String[] args) { // 얘가 없으면 프로그램이 스스로 실행되지 않는다
+		
+		/*
+		 * 여러줄 주석
+		 * 여러 줄을 사용 할때 쓴다
+		 * */
+		
+		boolean varBool = true;
+		
+//		여러 글자(더블 쿼터)
+		String varstr = "문자열"; 
+
+//		한글자(싱클쿼터)
+		char varchar = 's';
+		
+//		일반적인 정수
+		int varint = 1;
+		
+//		큰정수
+		long varlong = 10000000000000L;
+		
+//		큰 소숫점 (일반적으로 소숫점 5자리 이상)
+		double vardouble = 0.00001;
+		
+//		float 은 특수하게 f 를 붙여준다
+		float varfloat = 0.1f;
+		
+	}
+
+}
---
+++ MinMax_최소값_최대값.java
@@ -0,0 +1,21 @@
+package ex01;
+
+public class MinMax_최소값_최대값 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Byte : "+Byte.MIN_VALUE+" ~ "+Byte.MAX_VALUE);
+		
+		System.out.println("Short : "+Short.MIN_VALUE+" ~ "+Short.MAX_VALUE);
+		
+		System.out.println("int : "+Integer.MIN_VALUE+" ~ "+Integer.MAX_VALUE);
+		
+		System.out.println("Long : "+Long.MIN_VALUE+" ~ "+Long.MAX_VALUE);
+		
+		System.out.println("Float : "+Float.MIN_VALUE+" ~ "+Float.MAX_VALUE);
+		
+		System.out.println("Double : "+Double.MIN_VALUE+" ~ "+Double.MAX_VALUE);
+
+	}
+
+}
