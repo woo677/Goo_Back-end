d0dea25889207d1d88d8a4ec6293cb7bc223089c
황금 바바나 <63435073+woo677@users.noreply.github.com>
2024-02-26 23:51:09 +0900

    Java
    Java기본

---
+++ Chapter01_자바기본/src/ex01/Promotion_형변환.java
@@ -0,0 +1,36 @@
+package ex01;
+
+public class Promotion_형변환 {
+
+	public static void main(String[] args) {
+		// 작은거에서 큰걸로 갈땐 묵시적 형변환(Promotion)
+		
+		// 작은잔에서 큰잔으로 옮길때 당연히 안 흘릴테니 확인이 필요 없다.
+		
+		byte byteValue;
+		char charValue = 'a';
+		int intValue = 0;
+		long longValue;
+		float floatValue;
+		double doubleValue;
+
+		// byte < int
+		byteValue = 10;
+		intValue = byteValue;
+				
+		// char < int
+		intValue = charValue;
+		System.out.println(intValue);
+	// 97 -> ASCII CODE (각 문자의 변호가 공통 규격으로 정해져 있다.)
+		
+		//int < long
+		longValue = intValue;
+				
+		//float < double
+		floatValue = 0.1f;
+		doubleValue = floatValue;
+		
+
+	}
+
+}
