f097ec8b9a602fcaf22867aae10e47575efcb9f6
황금 바바나 <63435073+woo677@users.noreply.github.com>
2024-03-01 18:39:48 +0900

    Add files via upload

---
+++ JAVA/Chapter02/Compare.java
@@ -0,0 +1,34 @@
+
+public class Compare {
+
+	public static void main(String[] args) {
+		
+		// = 은 대입 연산으로 사용 중
+		int v1 = 1;
+		double v2 = 1.0;
+		int v3 = 4;
+		// 문자열은 여러 방법으로 선언 할 수 있다.
+		String v4 = "JAVA";
+		String v5 = new String("JAVA");
+		
+		// 비교 연산
+		System.out.println(v1==v2);
+		System.out.println(v1 != v2);
+		System.out.println(v1 < v3);
+		System.out.println(v4 == v5);
+		System.out.println(v4.equals(v5)); // 문자열 비교는 반드시 equals 로 해 주자
+		
+		String id = "admin";
+		String pw = "pass";
+		// 입력받은 id 와 pw 가 각각 admin 과 pass 면 로그인 완료 시켜주자
+		if (id.equals("admin") && pw.equals("pass")) {
+			System.out.println("login 되었습니다.");
+		}
+		
+		
+		
+		
+
+	}
+
+}
---
+++ JAVA/Chapter02/One.java
@@ -0,0 +1,55 @@
+
+public class One {
+
+	public static void main(String[] args) {
+
+		int x = 10;
+		int y = 10;
+		
+		x++; // 증가 나중에
+		++y; // 증가 먼저
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		
+		x = 1;
+		y = 1;
+		int result1 = (++x)+10;// x 가 먼저 증가-> 2 , 10을 더하면 = 12
+		int result2 = (y++)+10;// y와 10 을 더해서 11, 이후 y는 1 증가해서 2
+		System.out.println("result1 = "+result1);
+		System.out.println("result2 = "+result2);
+
+		// ++ 가 뒤에 붙는다.(아령들고 나서 1 세기) -> 카운트에 주로 활용
+		// ++ 가 앞에 붙는다.(증가한 내용을 이용해 다른행동을 할 때)
+		
+		// 반전 연산(! not)
+		boolean yn = true;
+		System.out.println("yn = "+yn);
+		
+		yn = !yn;
+		System.out.println("!yn = "+yn);
+		
+		yn = !yn;
+		System.out.println("!yn = "+yn);		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
