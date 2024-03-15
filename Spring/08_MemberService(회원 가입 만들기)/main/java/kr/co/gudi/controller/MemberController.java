f13063ba39080ff9aded729c8a4e5fc731301d9c
황금 바바나 <63435073+woo677@users.noreply.github.com>
2024-03-15 16:17:57 +0900

    로그인 페이지 만들기

--- Spring/08_MemberService(회원 가입 만들기)/main/java/kr/co/gudi/controller/MemberController.java
+++ Spring/08_MemberService(회원 가입 만들기)/main/java/kr/co/gudi/controller/MemberController.java
@@ -79,25 +79,24 @@ public class MemberController {
 	@RequestMapping(value = "/login")
 	public String login(Model model, String id,String pw,HttpSession session) { //Request 대신 이렇게 써도 된다 
 		
-			 String page = "login";
-			 String msg = "아이디 또는 비밀면호를 확인해 주세요";
+		String page = "login";
+		String msg = "아이디 또는 비밀면호를 확인해 주세요";
 			 
-			 logger.info(id+" / "+pw);
+		logger.info(id+" / "+pw);
 			 
-			 MemberService service = new MemberService();
+		MemberService service = new MemberService();
 			 
-			 if(service.login(id,pw)) {
-				 //"list"은 list.jsp파일로 이동하는 것
-				 //redirect:/list 는 같은 컨드롤러에 /list라는 요청이 있으면으면 거기로 이동을 한다
-				 page ="redirect:/list"; //list 라는 요청으로 이동시켜라 /list라는 곳으로 이동 시켜라
-				 //msg = id+"님 환영 합니다"; //redirect 사용시 model 의 값을 전달 할 수 없다
-				 session.setAttribute("loginId", id);
-			 }
+		if(service.login(id,pw)) {
+		//"list"은 list.jsp파일로 이동하는 것
+		//redirect:/list 는 같은 컨드롤러에 /list라는 요청이 있으면으면 거기로 이동을 한다
+		page ="redirect:/list"; //list 라는 요청으로 이동시켜라 /list라는 곳으로 이동 시켜라
+		//msg = id+"님 환영 합니다"; //redirect 사용시 model 의 값을 전달 할 수 없다
+		session.setAttribute("loginId", id);
+		}
+			 	 
+		model.addAttribute("msg",msg);
 			 
-			 
-			 model.addAttribute("msg",msg);
-			 
-		return page;
+	     return page;
 	}
 	
 	//회원 리스트
